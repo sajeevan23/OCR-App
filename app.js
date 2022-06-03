3cce3faaaf249f4bddb14992432661d796bfc8f8
Sajeevan.B <102434946+sajeevan23@users.noreply.github.com>
2022-06-03 10:31:55 +0530

    Update app.js
    new changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const worker = new TesseractWorker();
 const storage = multer.diskStorage({
     destination: (req, file,cb) => {
         cb(null, "./uploads");
-    },
+    },s
     filename: (res, file, cb) => {
         cb(null, file.originalname);
     }
@@ -21,20 +21,38 @@ const upload = multer({storage: storage}).single("avatar");
 
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+//Router
 
-//Route 
-
-app.get('/', (req,res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+    res.render("index")
 });
 
 app.post("/upload", (req, res) => {
     upload(req, res, err => {
-        console.log(req.file);
-    })
+        fs.readFile(`./uploads/${req.file.originalname}`, (err, Data) => {
+            if(err) return console.log('This is your error', err);
+
+            worker
+            .recongnize(Data, "eng", {tessjs_Create_pdf: '1'})
+            .progress(progress => {
+                console.log(progress);
+            })
+            .then(result => {
+                // res.send(result.text);
+                res.redirect('.download')
+            })
+            .finally(() => worker.terminate());
+        });
+    });
+});
+
+app.get('/download', (req,res) => {
+    const file = `${__dirname}/tesseract.js-ocr-result.pdf`;
+    res.download(file)
 })
 
-// start Up our server
 const PORT = 5000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Hey I'm running on port ❌💯✅ ${PORT}`));
+app.listen(PORT, () => console.log('Hey Im running on port ${PORT}'));
+
 
